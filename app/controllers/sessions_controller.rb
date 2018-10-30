04c71544e7c8166f6b26935e23a48fccc2fe606c
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-30 22:09:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :name
-    redirect_to controller: 'application', action 'hello'
+    redirect_to controller: 'application', action: 'hello'
   end
   
 end
