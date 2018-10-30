7ceb0ce150e9dc2b0bc2a7f55d00a48b734b2991
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-30 20:56:50 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    session[:name] = params[:name]
+    redirect_to '/login'
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/logout'
+  end
+  
+end
---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "homepage", :type => :view do
+  it "if the user is not logged in, the page should show them a login link." do
+    visit '/'
+    if !user.logged_in?
+      click_button 'login'
+      expect(page.body).to include "Username"
+    else
+      expect(page.body).to include "hi, #{name}"
+      click_button 'logout'
+    end
+  end
+  
+end
