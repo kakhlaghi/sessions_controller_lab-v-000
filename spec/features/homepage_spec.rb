6814d74904938827e6d0c823f30fce2680e2aa55
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-30 21:28:49 +0000

    automatically backed up by learn

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "homepage", :type => :view do
   it "if the user is not logged in, the page should show them a login link." do
+    
     if !session[:name]
-      visit '/login'
+      vis
       click_button 'login'
       expect(page.body).to include "Username"
     else
-      visit '/'
+      
 
       expect(page.body).to include "hi, #{name}"
       click_button 'logout'
